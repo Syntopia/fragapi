54bc81ba004fe86be559ce8e4bc24fdba0f1b7ea
Mikael Christensen <mikaelhc@gmail.com>
2015-07-25 20:53:18 +0200

    Updated

--- dist/fragapi-dev.js
+++ dist/fragapi-dev.js
@@ -1,13 +1,179 @@
+
+var Camera2D = function() {
+	this.mouseDownX = 0;
+	this.mouseX = 0;
+	this.mouseDownY = 0;
+	this.mouseY = 0;
+	this.centerX = 0;
+	this.centerY = 0;
+	
+	this.zoomFactor = 1.5;
+	this.zoom = 1.0;
+	this.dragFactor = 2.0;
+	
+	this.dragging = false;
+	this.dirty = false;
+	this.keyUp = false; 
+	this.keyDown = false;
+	this.keyLeft = false;
+	this.keyRight = false;
+	this.keyW = false;
+	this.keyS = false;
+};
+
+Camera2D.prototype = {
+
+	constructor: Camera2D,
+
+	init: function(canvasID) {
+		this.addListeners(canvasID);
+	},
+	
+	setCenter: function(x,y) {
+		this.centerX = x;
+		this.centerY = y;
+	},
+	
+	setZoom: function(z) {
+		this.zoom = z;
+	},
+	
+	mouseDown: function(e) {
+		var rect = this.canvas.getBoundingClientRect();
+        
+		this.mouseDownX = e.clientX - rect.left;
+		this.mouseDownY = e.clientY - rect.top;
+		this.dragging = true;
+		this.dirty = true;
+	},
+	
+	stopDragging: function() {
+		if (this.dragging) {
+			this.centerX -= (this.mouseX-this.mouseDownX)*this.zoom*this.dragFactor;
+			this.centerY += (this.mouseY-this.mouseDownY)*this.zoom*this.dragFactor;
+		}
+		this.dragging = false;
+		this.dirty = true;
+	},
+	
+	mouseUp: function() {
+		this.stopDragging();
+	},
+	
+	mouseWheel: function(ev) {
+        if (ev.detail<0.0) this.doZoom(this.zoomFactor);
+		if (ev.detail>0.0) this.doZoom(1/this.zoomFactor);
+		
+		if (ev.wheelDelta<0.0) this.doZoom(this.zoomFactor);
+		if (ev.wheelDelta>0.0) this.doZoom(1/this.zoomFactor);
+		
+        if (ev.preventDefault) ev.preventDefault();
+		ev.returnValue = false;
+		this.dirty = true;
+	},
+	
+	mouseMove: function(e) {
+		var rect = this.canvas.getBoundingClientRect();
+        
+		this.mouseX = e.clientX - rect.left;
+		this.mouseY = e.clientY - rect.top;
+		this.dirty = this.dirty || this.dragging; 
+		console.log("[centerX: " +this.centerX + " centerY: " +this.centerY + " zoom: " + this.zoom +"]");
+	},
+	
+	toggleStatus: function(keyCode, value, ev) {
+		switch( keyCode ) {
+			case 38: this.keyUp = value; break;
+			case 40: this.keyDown = value; break;
+			case 37: this.keyLeft = value; break;
+			case 39: this.keyRight = value; break;						
+			case 87: this.keyW = value; break;
+			case 83: this.keyS = value; break;
+			default: return;
+		}
+		if (ev.preventDefault) ev.preventDefault();
+		ev.returnValue = false;
+		
+	},
+	
+	getViewWidth: function() {
+		return this.canvas.getBoundingClientRect().width;
+	},
+	
+	getViewHeight: function() {
+		return this.canvas.getBoundingClientRect().height;
+	},
+	
+	doZoom: function(factor) {
+		var dx = this.mouseX-this.getViewWidth()/2;
+		var dy = this.getViewHeight()/2-this.mouseY;
+		this.centerX += dx*2*this.zoom*(1-factor);
+		this.centerY += dy*2*this.zoom*(1-factor);
+		this.zoom *= factor;
+	},
+	
+	checkKeys: function() {
+		if (this.keyDown || this.keyUp || this.keyLeft || this.keyRight || this.keyW || this.keyS) this.dirty = true;
+		if (this.keyW) this.doZoom(this.zoomFactor);
+		if (this.keyS) this.doZoom(1/this.zoomFactor);
+		if (this.keyDown) this.centerY += 10;
+		if (this.keyUp) this.centerY -= 10;
+		if (this.keyLeft) this.centerX += 10;
+		if (this.keyRight) this.centerX -= 10;
+	},
+	
+	setUniforms: function(frameBuffer) {
+		var w = frameBuffer.getWidth();
+		var h = frameBuffer.getHeight();
+		
+		var cx = this.centerX/w;
+		var cy = this.centerY/h;
+		
+		if (this.dragging) {
+			cx = (this.centerX-(this.mouseX-this.mouseDownX)*this.zoom*this.dragFactor)/w; 
+			cy = (this.centerY+(this.mouseY-this.mouseDownY)*this.zoom*this.dragFactor)/h;
+		}
+		frameBuffer.setUniform2f('center', cx, cy);
+		frameBuffer.setUniform1f('zoom', this.zoom);
+	},
+	
+	addListeners: function(canvasID) {
+		this.canvas = document.getElementById(canvasID);
+		
+		var self = this;
+		this.canvas.addEventListener('mousedown', function(e) { self.mouseDown(e); }, false);
+		this.canvas.addEventListener('mouseup', function(e) { self.mouseUp(e); }, false);
+		this.canvas.addEventListener('mouseout', function(e) { self.mouseUp(e); }, false); // check if dragging outside window.
+		this.canvas.addEventListener('mousemove', function(e) { self.mouseMove(e); }, false); 
+		this.canvas.addEventListener('mousewheel', function(e) { self.mouseWheel(e); }, false);
+		this.canvas.addEventListener('DOMMouseScroll', function(e) { self.mouseWheel(e); }, false);
+		
+		document.addEventListener('keydown', function ( event ) { self.toggleStatus(event.keyCode, true, event);}, false );
+		document.addEventListener('keyup',   function ( event ) { self.toggleStatus(event.keyCode, false, event);}, false );
+	}
+};
+
+Camera2D.vertexShader = 
+'attribute vec3 position;\n' +
+'varying vec2 pos;\n' +
+'varying vec2 canvasPos;\n' +
+'uniform float zoom;\n' +
+'uniform vec2 center;\n' +
+'\n' +
+'void main() {\n' +
+'	pos = position.xy * zoom + center;\n' +
+'	canvasPos = position.xy;\n' +
+'	gl_Position =  vec4(position, 1.0);\n' +
+'}\n';
 /**
- * @author Mikael Christensen / @SyntopiaDK
+ * @author Mikael Hvidtfeldt Christensen / twitter: @SyntopiaDK, web: hvidtfeldts.net
  */
 
 var fragapi = {
-    REVISION: '0'
+    REVISION: '0.0.0'
 };
 
-// TODO: Don't force this upon API users.
-// Strategy outlined in https://cvs.khronos.org/svn/repos/registry/trunk/public/webgl/sdk/demos/common/webgl-utils.js
+// Taken from: https://cvs.khronos.org/svn/repos/registry/trunk/public/webgl/sdk/demos/common/webgl-utils.js
 window.requestAnimFrame = (function() {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -30,7 +196,7 @@ fragapi.getGlFromCanvas = function(canvasID) {
             alpha: false
         };
         gl = canvas.getContext("experimental-webgl", settings);
-        if (gl == null) {
+        if (gl === null) {
             gl = canvas.getContext('webgl', settings);
         }
     } catch (e) {}
@@ -40,13 +206,13 @@ fragapi.getGlFromCanvas = function(canvasID) {
     gl.viewportWidth = canvas.width;
     gl.viewportHeight = canvas.height;
     return gl;
-}
+};
 
 fragapi.pendingOperations = [];
 fragapi.externalShaders = new Map();
 fragapi.pendingOperationsCounter = 0;
 
-fragapi.loadScript = function(src, id, callback) {
+fragapi.loadScript = function(src, callback) {
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -54,7 +220,7 @@ fragapi.loadScript = function(src, id, callback) {
     script.src = src;
     head.appendChild(script);
     fragapi.pendingOperations.push(src);
-}
+};
 
 fragapi.injectShader = function() {
     if (fragapi.pendingOperations.length != 1) {
@@ -63,10 +229,10 @@ fragapi.injectShader = function() {
     var src = fragapi.pendingOperations.pop();
     console.log("Retrieved shader: " + src + " %o", fragapi._externalShader.split("\n").join().substring(0, 150));
     fragapi.externalShaders.set(src, fragapi._externalShader);
-}
+};
 
 fragapi.getShader = function(gl, source, type) {
-    shader = gl.createShader(type);
+    var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
 
@@ -78,7 +244,7 @@ fragapi.getShader = function(gl, source, type) {
     }
 
     return shader;
-}
+};
 
 // Some functions used from: https://developer.mozilla.org/en/WebGL/Adding_2D_content_to_a_WebGL_context
 fragapi.getShaderFromElement = function(gl, id) {
@@ -100,26 +266,35 @@ fragapi.getShaderFromElement = function(gl, id) {
     }
 
     return fragapi.getShader(gl, source, shaderScript.type);
-}
+};
 
-FrameBuffer = function() {
+var FrameBuffer = function() {
     this.name = "Undefined";
     this.fragmentShader = "";
     this.vertexShader = "";
-}
+};
 
 FrameBuffer.create3D = function(fragmentShader) {
     var fb = new FrameBuffer();
     fb.fragmentShader = fragmentShader;
     return fb;
-}
+};
 
 FrameBuffer.create = function(fragmentShader, vertexShader) {
     var fb = new FrameBuffer();
     fb.fragmentShader = fragmentShader;
     fb.vertexShader = vertexShader;
     return fb;
-}
+};
+
+FrameBuffer.create2D = function(fragmentShader) {
+    var fb = new FrameBuffer();
+    fb.fragmentShader = fragmentShader;
+    fb.vertexShaderSource = Camera2D.vertexShader;
+	var cam = new Camera2D();
+	fb.camera = cam;
+    return fb;
+};
 
 FrameBuffer.prototype = {
 
@@ -184,7 +359,7 @@ FrameBuffer.prototype = {
         }
 
         var text = this.getFromElement(src);
-        if (text != null) {
+        if (text !== null) {
             console.log("Resolved " + src + " from element");
             return text;
         }
@@ -194,7 +369,7 @@ FrameBuffer.prototype = {
         }
 
         var self = this;
-        fragapi.loadScript(src, src, function() {
+        fragapi.loadScript(src, function() {
             self.initShaders();
         });
     },
@@ -206,8 +381,11 @@ FrameBuffer.prototype = {
             return;
         }
 
-        if ((this.vertexShaderSource == null || this.fragmentShaderSource == null) && fragapi.pendingOperations.length == 0) {
+        if (this.vertexShaderSource === undefined && fragapi.pendingOperations.length === 0) {
             this.vertexShaderSource = this.resolve(this.vertexShader);
+        }
+		
+		if (this.fragmentShaderSource === undefined && fragapi.pendingOperations.length === 0) {
             this.fragmentShaderSource = this.resolve(this.fragmentShader);
         }
 
@@ -218,7 +396,7 @@ FrameBuffer.prototype = {
             fragapi.pendingOperationsCounter++;
 
             window.setTimeout(function() {
-                self.initShaders()
+                self.initShaders();
             }, 300);
             return;
         }
@@ -274,8 +452,6 @@ FrameBuffer.prototype = {
         }
         console.log("Drawing frame");
         var gl = this.gl;
-        var w = gl.viewportWidth * 0.5;
-        var h = gl.viewportHeight * 0.5;
         gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVertexPositionBuffer);
@@ -298,7 +474,7 @@ FrameBuffer.prototype = {
         if (this.camera) {
             this.camera.checkKeys();
             if (this.camera.dirty || this.dirty) {
-                this.camera.setUniforms(fb);
+                this.camera.setUniforms(this);
                 this.camera.dirty = false;
                 this.dirty = true;
             }
@@ -326,8 +502,11 @@ FrameBuffer.prototype = {
         this.animate();
     },
 
-    renderToCanvas: function(name) {
-        this.gl = fragapi.getGlFromCanvas(name);
+    renderToCanvas: function(canvasID) {
+        this.gl = fragapi.getGlFromCanvas(canvasID);
+		if (this.camera) {
+			this.camera.init(canvasID);
+		} 
         this.init();
     },
 
@@ -338,145 +517,4 @@ FrameBuffer.prototype = {
     getHeight: function() {
         return this.gl.viewportHeight;
     }
-}
-
-
-Camera2D = function() {
-	this.mouseDownX = 0;
-	this.mouseX = 0;
-	this.mouseDownY = 0;
-	this.mouseY = 0;
-	this.centerX = 0;
-	this.centerY = 0;
-	
-	this.zoomFactor = 2.0;
-	this.zoom = 1.0;
-	this.dragFactor = 2.0;
-	
-	this.dragging = false;
-	this.dirty = false;
-	this.keyUp = false; 
-	this.keyDown = false;
-	this.keyLeft = false;
-	this.keyRight = false;
-	this.keyW = false;
-	this.keyS = false;
-}
-
-Camera2D.create = function(canvasID) {
-	var cam = new Camera2D();
-	cam.addListeners(canvasID);
-	return cam;
-}
-
-Camera2D.prototype = {
-
-	constructor: Camera2D,
-
-	mouseDown: function(e) {
-		var rect = canvas.getBoundingClientRect();
-        
-		this.mouseDownX = e.clientX - rect.left;
-		this.mouseDownY = e.clientY - rect.top;
-		this.dragging = true;
-		this.dirty = true;
-	},
-	
-	stopDragging: function() {
-		if (this.dragging) {
-			this.centerX -= (this.mouseX-this.mouseDownX)*this.zoom*this.dragFactor;
-			this.centerY += (this.mouseY-this.mouseDownY)*this.zoom*this.dragFactor;
-		}
-		this.dragging = false;
-		this.dirty = true;
-	},
-	
-	mouseUp: function(e) {
-		this.stopDragging();
-	},
-	
-	mouseWheel: function(ev) {
-        if (ev.detail<0.0) this.doZoom(this.zoomFactor);
-		if (ev.detail>0.0) this.doZoom(1/this.zoomFactor);
-		
-		if (ev.wheelDelta<0.0) this.doZoom(this.zoomFactor);
-		if (ev.wheelDelta>0.0) this.doZoom(1/this.zoomFactor);
-		
-        if (ev.preventDefault) ev.preventDefault();
-		ev.returnValue = false;
-		this.dirty = true;
-	},
-	
-	mouseMove: function(e) {
-		var rect = canvas.getBoundingClientRect();
-        
-		this.mouseX = e.clientX - rect.left;
-		this.mouseY = e.clientY - rect.top;
-		this.dirty = this.dirty || this.dragging; 
-		console.log("Move: x: " +this.mouseX + " y: " +this.mouseY);
-	},
-	
-	toggleStatus: function(keyCode, value, ev) {
-		switch( keyCode ) {
-			case 38: this.keyUp = value; break;
-			case 40: this.keyDown = value; break;
-			case 37: this.keyLeft = value; break;
-			case 39: this.keyRight = value; break;						
-			case 87: this.keyW = value; break;
-			case 83: this.keyS = value; break;
-			default: return;
-		}
-		if (ev.preventDefault) ev.preventDefault();
-		ev.returnValue = false;
-		
-	},
-	
-	doZoom: function(factor) {
-		dx = this.mouseX-300;
-		dy = 300-this.mouseY;
-		this.centerX += dx*2*this.zoom*(1-factor);
-		this.centerY += dy*2*this.zoom*(1-factor);
-		this.zoom *= factor;
-	},
-	
-	checkKeys: function() {
-		if (this.keyDown || this.keyUp || this.keyLeft || this.keyRight || this.keyW || this.keyS) this.dirty = true;
-		if (this.keyW) this.doZoom(this.zoomFactor);
-		if (this.keyS) this.doZoom(1/this.zoomFactor);
-		if (this.keyDown) this.centerY += 10;
-		if (this.keyUp) this.centerY -= 10;
-		if (this.keyLeft) this.centerX += 10;
-		if (this.keyRight) this.centerX -= 10;
-	},
-	
-	setUniforms: function(frameBuffer) {
-		var w = frameBuffer.getWidth();
-		var h = frameBuffer.getHeight();
-		
-		var cx = this.centerX/w;
-		var cy = this.centerY/h;
-		
-		if (this.dragging) {
-			cx = (this.centerX-(this.mouseX-this.mouseDownX)*this.zoom*this.dragFactor)/w; 
-			cy = (this.centerY+(this.mouseY-this.mouseDownY)*this.zoom*this.dragFactor)/h;
-		}
-		frameBuffer.setUniform2f('center', cx, cy);
-		frameBuffer.setUniform1f('zoom', this.zoom);
-	},
-	
-	addListeners: function(canvasID) {
-		this.canvas = document.getElementById(canvasID);
-		
-		var self = this;
-		this.canvas.addEventListener('mousedown', function(e) { self.mouseDown(e) }, false);
-		this.canvas.addEventListener('mouseup', function(e) { self.mouseUp(e) }, false);
-		this.canvas.addEventListener('mouseout', function(e) { self.mouseUp(e) }, false); // check if dragging outside window.
-		this.canvas.addEventListener('mousemove', function(e) { self.mouseMove(e) }, false); 
-		this.canvas.addEventListener('mousewheel', function(e) { self.mouseWheel(e) }, false);
-		this.canvas.addEventListener('DOMMouseScroll', function(e) { self.mouseWheel(e) }, false);
-		
-		document.addEventListener('keydown', function ( event ) { self.toggleStatus(event.keyCode, true, event);}, false );
-		document.addEventListener('keyup',   function ( event ) { self.toggleStatus(event.keyCode, false, event);}, false );
-	}
-	
-}
+};
